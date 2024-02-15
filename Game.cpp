63806659a98449731f049581cd9804ab29829dd5
adeumain <114288505+adeumain@users.noreply.github.com>
2024-02-15 19:39:23 +0100

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -128,3 +128,48 @@ void Game::attaque(Joueur& joueur, int cible, int degats, int hp, int porte) {
         verifParade(cible, degats, hp, demande);
     }
 }
+
+void Game::afficherStats(Joueur& joueur) {
+        std::cout << "Nom: " << joueur.getName() << std::endl;
+        std::cout << "Role: " << joueur.getRole() << std::endl;
+        std::cout << "Personnage: " << joueur.getCharacter() << std::endl;
+        std::cout << "Vie: " << joueur.getVie() << std::endl;
+        std::cout << "Honneur: " << joueur.getHoneur() << std::endl;
+        std::cout << "Main: " << std::endl;
+}
+
+void Game::afficherMain(Joueur& joueur) {
+    for (int j = 0; j < joueur.getMain().size(); j++) {
+        std::cout << j + 1 << ". " << joueur.getMain()[j].getName() << std::endl;
+    }
+}
+
+void Game::afficherJoueurs() {
+    for (Joueur& joueur : ListeJoueur) {
+        int i = 0;
+        std::cout << "Nom: " << joueur.getName() << i + 1 << std::endl;
+        std::cout << "Role: " << joueur.getRole() << std::endl;
+        std::cout << "Personnage: " << joueur.getCharacter() << std::endl;
+        std::cout << "Vie: " << joueur.getVie() << std::endl;
+        std::cout << "Honneur: " << joueur.getHoneur() << std::endl;
+        i++;
+
+    }
+}
+
+void Game::selectionCarte(int i) {
+    int choix = 0;
+    int cible = 0;
+    std::cin >> choix;
+    std::cout << "Choisissez la carte à jouer: ";
+    std::cout << "Vous avez choisi: " << ListeJoueur[i].getMain()[choix - 1].getName() << std::endl;
+    // Entrer la variable choix dans le vector choixCarte
+    defausse.push_back(ListeJoueur[i].getMain()[choix - 1]);
+    // Supprimer la carte jouée de la main du joueur
+    ListeJoueur[i].getMain().erase(ListeJoueur[i].getMain().begin() + choix - 1);
+    // Saisi le numéro de la cible
+    std::cout << "Choisissez la cible: ";
+    std::cin >> cible;
+    // Si la cible est un joueur
+
+    }
